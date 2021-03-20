09a81bbbab41ba4d93a84d0df60cd46fabcce188
xMiCx <70638947+Mickuppom@users.noreply.github.com>
2021-03-20 15:54:52 +0700

    Update home_screen.dart

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -41,49 +41,51 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text("QR Code Scaner"),
       ),
-      body: Container(
-        child: Column(
-          children: [
-            Text("Press Scan Button"),
-            Stack(
-              children: [
-                TextFormField(
-                  controller: input,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                  ),
-                  maxLines: 12,
-                ),
-                Positioned(
-                  top: 10,
-                  right: 10,
-                  child: Column(
+      body: !isCameraPermissionEnabled
+          ? Center()
+          : Container(
+              child: Column(
+                children: [
+                  Text("Press Scan Button"),
+                  Stack(
                     children: [
-                      ElevatedButton.icon(
-                        onPressed: copy,
-                        icon: Icon(Icons.copy),
-                        label: Text("Copy"),
+                      TextFormField(
+                        controller: input,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
+                        maxLines: 12,
                       ),
-                      ElevatedButton.icon(
-                        onPressed: share,
-                        icon: Icon(Icons.share),
-                        label: Text("Share"),
+                      Positioned(
+                        top: 10,
+                        right: 10,
+                        child: Column(
+                          children: [
+                            ElevatedButton.icon(
+                              onPressed: copy,
+                              icon: Icon(Icons.copy),
+                              label: Text("Copy"),
+                            ),
+                            ElevatedButton.icon(
+                              onPressed: share,
+                              icon: Icon(Icons.share),
+                              label: Text("Share"),
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
-                ),
-              ],
-            ),
-            Container(
-              child: ElevatedButton.icon(
-                onPressed: scan,
-                icon: Icon(Icons.camera),
-                label: Text("Scan"),
+                  Container(
+                    child: ElevatedButton.icon(
+                      onPressed: scan,
+                      icon: Icon(Icons.camera),
+                      label: Text("Scan"),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
     );
   }
 }
